98f255f606003c3cbb228361810e2f253cc7b130
jessi <jcoplen2@ivytech.edu>
2025-07-19 11:38:32 -0400

    backend

---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://jcoplen2:Password255@songdb.2lhticg.mongodb.net/?retryWrites=true&w=majority&appName=SongDB")
+
+module.exports = mongoose
